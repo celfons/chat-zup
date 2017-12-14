ea2e9881d7cc1fb3759d38f0da850a97b440857f
celfons <marcel.resende@yahoo.com.br>
2017-12-14 18:18:49 -0200

    fix

--- controllers.js
+++ controllers.js
@@ -30,7 +30,7 @@ $scope.logout = function() {
   document.location.href = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://celfons.marcelfonseca.com.br/chat-zup/#/";
 }
 
-$scope.user = 'Usuário Anônimo';
+$scope.user = 'user';
 
 var d   = new Date();
 var dtF = (d.getDate() < 10 ? "0" : "") + d.getDate() + "/" + (d.getMonth() + 1 < 10 ? "0" : "") + (d.getMonth() + 1) + "/" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
